6f10256b3c9bc9707ed4f98fd7ec89546575a20c
Leandro-Conde <leandro27conde@gmail.com>
2026-02-09 20:59:02 -0300

    java1

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
+//elementos dom
 const form = document.querySelector("#task");
 const taskList = document.querySelector("#task-list"); //não vai mudar
+const titulo = document.querySelector("#titulo");
+const prioridade = document.querySelector ("#prioridade");
 
+//estado da aplicação
 let tasks = []; //vai mudar
 
+//formulário
 form.addEventListener("submit", (Event) => {
-    Event.preventDefault();
-    alert("Formulário enviado");
+    Event.preventDefault();//reload(impede)
+
+    const title = titulo.value.trim();
+    const priority = prioridade.value;
+
+    //validação
+    if (title === "") {
+        alert("Digite um título");
+        return;
+    }
+
+    //array
+    const newTask = {
+        titulo: title,
+        prioridade: priority
+    };
+    tasks.push(newTask);
+
+    //Atualiza a tela
+    renderTasks();
+
+    //limpa o form
+    titulo.value = "";
+    prioridade.value= "media";
 });
+
+//renderização
+function renderTasks() {
+    taskList.innerHTML = "";
+
+    tasks.forEach((task) =>{
+        const li = document.createElement("li");
+
+        li.textContent = `${task.titulo} (${task.prioridade})`;
+
+        taskList.appendChild(li);
+    });
+}
